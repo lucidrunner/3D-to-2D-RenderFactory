dc2f87001dd21c510cfd3f6c19be6509c73d9ed0
Björn Rundquist <bjorn.rundqvist@gmail.com>
2021-11-01 14:55:04 +0100

    Missed files in last commit

---
+++ Assets/Editor/Factory Editor/CameraRigEditor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Render3DTo2D.Rigging;
+using Shared_Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace Factory_Editor
+{
+    [CustomEditor(typeof(CameraRig))]
+    public class CameraRigEditor : Editor
+    {
+        private SerializedProperty customPlacementModeProp;
+        private SerializedProperty rigTypeProp;
+        
+        private void OnEnable()
+        {
+            customPlacementModeProp = serializedObject.FindProperty("manualPlacementMode");
+            rigTypeProp = serializedObject.FindProperty("rigType");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            //If we're not doing custom placement, draw the script as normal
+            if(customPlacementModeProp.boolValue == false)
+            {
+                base.OnInspectorGUI();
+                return;
+            }
+
+            //Otherwise, we'll need to manually draw it to add camera adding / removing & validation
+            GUI.enabled = false;
+            EditorGUILayout.ObjectField("Script:", MonoScript.FromMonoBehaviour((CameraRig)target), typeof(CameraRig), false);
+            GUI.enabled = true;
+            var _target = (CameraRig)target;
+
+
+            string _rigType = rigTypeProp.enumDisplayNames[rigTypeProp.enumValueIndex];
+            //First, show the set mode for the rig since this will influence validation
+            EditorColors.OverrideTextColors();
+            EditorGUILayout.LabelField($"Camera Placement Mode: {_rigType}");
+            
+            EditorColors.ResetTextColor();
+
+        }
+    }
+
+}
